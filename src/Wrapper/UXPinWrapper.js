8699c73f36c29de121f9caa1eef1ffc89bb2ed20
Derek Haynes <derek.haynes@gmail.com>
2020-09-24 08:13:00 -0600

    Theme example

--- src/Wrapper/UXPinWrapper.js
+++ src/Wrapper/UXPinWrapper.js
@@ -1,6 +1,9 @@
 import React from "react";
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Uncomment to use sb-admin theme.
+// https://startbootstrap.com/themes/sb-admin-2/
+import '../sb-admin-2.css';
 
 export default function UXPinWrapper({ children }) {
   return children;
