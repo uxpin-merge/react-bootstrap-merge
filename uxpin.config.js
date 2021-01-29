9141719413a06eaeb480fb3709fe811883c157dd
Naohiro Takahashi <naohiro@xenon.io>
2021-01-29 09:52:38 +0900

    storybook

--- uxpin.config.js
+++ uxpin.config.js
@@ -4,19 +4,9 @@ module.exports = {
       {
         name: 'General',
         include: [
-          'src/Button/Button.js',
+          'src/stories/Button.stories.js',
         ]
       },
-      {
-        name: 'Forms',
-        include: [
-          'src/Form/Form.js',
-          'src/FormGroup/FormGroup.js',
-          'src/FormLabel/FormLabel.js',
-          'src/FormControl/FormControl.js',
-
-        ]
-      }
     ],
     wrapper: 'src/Wrapper/UXPinWrapper.js',
     webpackConfig: 'uxpin.webpack.config.js',
